7c49d95f98e662be9e1c7725f8708b08a3ccd71f
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2022-03-24 02:20:24 +0100

    rename main function as its no longer accurate

--- FTH.user.js
+++ FTH.user.js
@@ -6,7 +6,7 @@
 // @match        https://twitter.com/*
 // @icon         https://www.google.com/s2/favicons?domain=twitter.com
 // @grant        none
-// @version      1.0.2
+// @version      1.0.3
 // @updateURL https://raw.githubusercontent.com/Blumlaut/FuckTwitterHome/main/FTH.user.js
 // @downloadURL https://raw.githubusercontent.com/Blumlaut/FuckTwitterHome/main/FTH.user.js
 // ==/UserScript==
@@ -16,24 +16,23 @@
 
     var currentTry=0
 
-    function findStupidFuckingAlgoToggle() {
+    function init() {
         if (currentTry==3) {
             console.log("looks like the algo spared us...")
             return
         }
-        
+
         var communitiesButton = document.querySelector('[aria-label="Communities"]')
         if (communitiesButton) {
             communitiesButton.remove()
         }
-        
         var timelineModeButton = document.querySelector('[aria-label="Top Tweets on"]')
         if (!timelineModeButton) {
-            setTimeout(findStupidFuckingAlgoToggle, 5000)
+            setTimeout(init, 5000)
             currentTry++
         } else {
             timelineModeButton.click()
-            
+
             function fuckingToggleTimeline() {
                 var stupidFuckingToggleButton = document.querySelectorAll('[role="menuitem"]')[0]
                 if (!stupidFuckingToggleButton) {
@@ -47,6 +46,6 @@
 
         }
     }
-    setTimeout(findStupidFuckingAlgoToggle, 3000)
+    setTimeout(init, 3000)
 
 })();
